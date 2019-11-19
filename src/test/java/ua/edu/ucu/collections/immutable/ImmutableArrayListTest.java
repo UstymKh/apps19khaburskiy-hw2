ef0ac30cf63d78ca64e6238f481828e30e40190f
ZustymZ <ustymmvd@gmail.com>
2019-11-19 23:36:08 +0200

    New version

--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -2,8 +2,7 @@ package ua.edu.ucu.collections.immutable;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ImmutableArrayListTest {
 
@@ -73,9 +72,105 @@ public class ImmutableArrayListTest {
         assertEquals(expectedResult, realResult);
     }
 
+    @Test
+    public void testSet() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5, 4};
+        realResult = testImmutableArrayList.set(1, 4).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testSize() {
+        Object expectedResult;
+        Object realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = 2;
+        realResult = testImmutableArrayList.size();
+        assertEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testRemove() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5};
+        realResult = testImmutableArrayList.remove(1).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testClear() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[0];
+        realResult = testImmutableArrayList.clear().toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        boolean expectedResult;
+        boolean realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = true;
+        realResult = testImmutableArrayList.isEmpty();
+        assertNotEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testIndexOf() {
+        Object expectedResult;
+        Object realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = 0;
+        realResult = testImmutableArrayList.indexOf(5);
+        assertEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5};
+        realResult = testImmutableArrayList.remove(1).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
     @Test(expected = IndexOutOfBoundsException.class)
     public void testAddIndexError() {
         ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
         testImmutableArrayList.add(3, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveIndexError() {
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList.remove(3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetIndexError() {
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList = testImmutableArrayList.add(7);
+        testImmutableArrayList.set(3, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllIndexError() {
+        ImmutableArrayList testImmutableArrayList = new ImmutableArrayList();
+        testImmutableArrayList.addAll(3, new Object[]{4, 5});
     }
 }
--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableLinkedListTest.java
@@ -2,8 +2,7 @@ package ua.edu.ucu.collections.immutable;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ImmutableLinkedListTest {
 
@@ -77,5 +76,107 @@ public class ImmutableLinkedListTest {
     public void testaddIndexError() {
         ImmutableLinkedList testImmutableLinkedList = new ImmutableLinkedList();
         testImmutableLinkedList.add(3, 1);
+    }
+
+    @Test
+    public void testSet() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5, 4};
+        realResult = testImmutableArrayList.set(1, 4).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testSize() {
+        Object expectedResult;
+        Object realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = 2;
+        realResult = testImmutableArrayList.size();
+        assertEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testRemove() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5};
+        realResult = testImmutableArrayList.remove(1).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testClear() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[0];
+        realResult = testImmutableArrayList.clear().toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        boolean expectedResult;
+        boolean realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = true;
+        realResult = testImmutableArrayList.isEmpty();
+        assertNotEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testIndexOf() {
+        Object expectedResult;
+        Object realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = 0;
+        realResult = testImmutableArrayList.indexOf(5);
+        assertEquals(expectedResult, realResult);
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] expectedResult;
+        Object[] realResult;
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(new Object[]{5, 3});
+        expectedResult = new Object[]{5};
+        realResult = testImmutableArrayList.remove(1).toArray();
+        assertArrayEquals(expectedResult, realResult);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddIndexError() {
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList.add(3, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveIndexError() {
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList.remove(3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetIndexError() {
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList = testImmutableArrayList.add(7);
+        testImmutableArrayList.set(3, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllIndexError() {
+        ImmutableLinkedList testImmutableArrayList = new ImmutableLinkedList();
+        testImmutableArrayList.addAll(3, new Object[]{4, 5});
     }
 }
